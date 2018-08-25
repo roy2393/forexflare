067cb55cb2dd37beb1df4b2cd1ada41782511a78
Rohit Roy <you@example.com>
2018-08-25 11:30:08 +0530

    feature implemented

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,8 @@
-export const FIREBASE_API_KEY = 'AIzaSyAw3R0RA74d7rRt1e8qcIEi3x9vdV2grQE';
-export const FIREBASE_AUTH_DOMAIN = 'https://forex-flares.firebaseapp.com/__/auth/handler';
-export const FIREBASE_DATABASE_URL = 'https://forex-flares.firebaseio.com/';
-export const FIREBASE_PROJECT_ID = 'forex-flares';
-export const FIREBASE_STORAGE_BUCKET = 'gs://forex-flares.appspot.com';
+export const FIREBASE_API_KEY = '';
+export const FIREBASE_AUTH_DOMAIN = '';
+export const FIREBASE_DATABASE_URL = '';
+export const FIREBASE_PROJECT_ID = '';
+export const FIREBASE_STORAGE_BUCKET = '';
 // export const FIREBASE_MESSAGING_SENDER_ID = 
 
-export const FACEBOOK_APP_ID = 2180359042195881;
+export const FACEBOOK_APP_ID = 0;
