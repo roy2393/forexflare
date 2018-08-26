81e0e29719de0df57359d68e5434c3d13d99a6cd
Divya <divya.gangwar@games24x7.com>
2018-08-26 17:14:06 +0530

    ios changes - initial commit

---
+++ ios/Pods/Target Support Files/Pods-ForexFlaresApp-tvOS/Pods-ForexFlaresApp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForexFlaresApp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ForexFlaresApp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-ForexFlaresApp/Pods-ForexFlaresApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ForexFlaresApp : NSObject
+@end
+@implementation PodsDummy_Pods_ForexFlaresApp
+@end
